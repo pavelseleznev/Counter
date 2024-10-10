246ccbc258724755bca6903467e91e79f469204e
PavelSeleznev <seleznevpm@icloud.com>
2024-10-10 23:26:00 +0300

    Implemented code review changes

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,60 +10,58 @@ import UIKit
 final class ViewController: UIViewController {
     
     // MARK: - IB Outlets
-    @IBOutlet weak private var counterLabel: UILabel! // Label shows the counter value
-    @IBOutlet weak private var statusTextView: UITextView! // textView displays the status updates after changing the counter value
+    /// Label shows the counter value
+    @IBOutlet weak private var counterLabel: UILabel!
+    /// textView displays the status updates after changing the counter value
+    @IBOutlet weak private var statusTextView: UITextView!
     
     // MARK: - Private Properties
-    lazy private var labelCount: Int = 0 // Variable stores counter's value
-    private let now = Date() //
+    /// Variable stores counter's value
+    lazy private var labelCount: Int = 0
+    private let now = Date()
     private let formatter = DateFormatter()
-    
-    
-    // MARK: - IB Actions
-    @IBAction private func increaseCountButton(_ sender: Any) { // Button increases the counter's value & adds the status information in the statusTextView
-        if labelCount >= 0 {
-            labelCount += 1
-            formatTimeAndDate()
-            let dateString = formatter.string(from: now)
-            statusTextView.text.append("\n" + "[\(dateString)]: " + "значение изменено на +1")
-            statusTextView.scrollToBottom()
-        }
-        counterLabel.text = String("Значение счётчика: \(labelCount)")
-    }
-    
-    @IBAction private func decreaseCountButton(_ sender: Any) { // Button decreases the counter's value & adds the status information in the statusTextView
-        if labelCount > 0 {
-            labelCount -= 1
-            formatTimeAndDate()
-            let dateString = formatter.string(from: now)
-            statusTextView.text.append("\n" + "[\(dateString)]: " + "значение изменено на -1")
-            statusTextView.scrollToBottom()
-        } else {
-            formatTimeAndDate()
-            let dateString = formatter.string(from: now)
-            statusTextView.text.append("\n" + "[\(dateString)]: " + "попытка уменьшить значение счётчика ниже 0")
-            statusTextView.scrollToBottom()
-        }
-        counterLabel.text = String("Значение счётчика: \(labelCount)")
-    }
-    
-    @IBAction private func resetCountButton(_ sender: Any) { // Button resets the counter's value to 0 & adds the status information in the statusTextView
-        labelCount = 0
-        counterLabel.text = String("Значение счётчика: \(0)")
-        formatTimeAndDate()
-        let dateString = formatter.string(from: now)
-        statusTextView.text.append("\n" + "[\(dateString)]: " + "значение сброшено")
-        statusTextView.scrollToBottom()
-    }
     
     // MARK: - Private Method
     private func formatTimeAndDate() {
         formatter.timeZone = TimeZone.current
         formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        statusTextView.scrollToBottom()
+    }
+    
+    // MARK: - IB Actions
+    /// Button increases the counter's value & adds the status information in the statusTextView
+    @IBAction private func increaseCountButton(_ sender: Any) {
+        formatTimeAndDate()
+        labelCount += 1
+        let dateString = formatter.string(from: now)
+        statusTextView.text.append("\n" + "[\(dateString)]: " + "значение изменено на +1")
+        counterLabel.text = String("Значение счётчика: \(labelCount)")
+    }
+    
+    /// Button decreases the counter's value & adds the status information in the statusTextView
+    @IBAction private func decreaseCountButton(_ sender: Any) {
+        formatTimeAndDate()
+        let dateString = formatter.string(from: now)
+        if labelCount > 0 {
+            labelCount -= 1
+            statusTextView.text.append("\n" + "[\(dateString)]: " + "значение изменено на -1")
+        } else {
+            statusTextView.text.append("\n" + "[\(dateString)]: " + "попытка уменьшить значение счётчика ниже 0")
+        }
+        counterLabel.text = String("Значение счётчика: \(labelCount)")
+    }
+    
+    /// Button resets the counter's value to 0 & adds the status information in the statusTextView
+    @IBAction private func resetCountButton(_ sender: Any) {
+        formatTimeAndDate()
+        counterLabel.text = String("Значение счётчика: \(0)")
+        let dateString = formatter.string(from: now)
+        statusTextView.text.append("\n" + "[\(dateString)]: " + "значение сброшено")
     }
 }
 
-private extension UITextView { // This extension provides the scrollToBottom functionality for the statusTextView
+/// This extension provides the scrollToBottom functionality for the statusTextView
+private extension UITextView {
     func scrollToBottom() {
         let textCount: Int = text.count
         guard textCount >= 1 else { return }
